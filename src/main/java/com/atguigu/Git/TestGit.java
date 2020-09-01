db135f888ebc1c6ddb4a5c2675a9c3e1df27477c
RunningBoy <2239517047@qq.com>
2020-09-01 19:20:22 +0800

    test master commit

--- src/main/java/com/atguigu/Git/TestGit.java
+++ src/main/java/com/atguigu/Git/TestGit.java
@@ -3,7 +3,7 @@ package com.atguigu.Git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello");//edit by master
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
