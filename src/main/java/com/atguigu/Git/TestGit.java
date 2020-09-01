1080f652d4e9e0d45b6c4aecfc379d27d62284ab
RunningBoy <2239517047@qq.com>
2020-09-01 19:31:46 +0800

    master 56767

--- src/main/java/com/atguigu/Git/TestGit.java
+++ src/main/java/com/atguigu/Git/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");//edit by hotfix
-        System.out.println("hello");//edit by master
+        System.out.println("hello");//edit by master3432
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
